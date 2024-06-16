2b3144c9097004e956a72e1bf6ac09d69a7c2a17
idezs <76953137+idezs@users.noreply.github.com>
2024-06-16 22:41:50 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -14,9 +14,7 @@ def main_menu():
         inquirer.List(
             "YOU SELECTED",
             message="WHAT YOU NEED TO DO?",
-            choices=["SERIAL_INITIAL_SWITCH_IOS_DEVICES", "PM_CGH_LLK_AUTOMATION_JUNIPER", "PM_CGH_SAIMAI_AUTOMATION_JUNIPER",
-             "PM_CGH_PAHOLYOTHIN_AUTOMATION_JUNIPER", "PM_SCIPRK_AUTOMATION_CISCO",
-              "PM_RMUTTO_AUTOMATION_CISCO", "PM_TU_TAPRACHAN_AUTOMATION_CISCO"],
+            choices=["PM_AUTOMATION_CISCO_1", "PM_AUTOMATION_CISCO_2"],
         ),
     ]
     
