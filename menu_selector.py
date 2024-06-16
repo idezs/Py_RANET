909b70938efe25cfb2b4a8c7141b862ace3d9a9b
idezs <76953137+idezs@users.noreply.github.com>
2024-06-16 22:40:29 +0700

    Update menu_selector.py

--- menu_selector.py
+++ menu_selector.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 import cmd_collector
 
 def icon_selector(site):
-    if site == "PM_CGH_LLK_AUTOMATION_JUNIPER":
+    if site == "PM_AUTOMATION_CISCO_1":
         cmd_collector.cisco_pm_function(site)
-    elif site == "PM_CGH_SAIMAI_AUTOMATION_JUNIPER":
+    elif site == "PM_AUTOMATION_CISCO_2":
         cmd_collector.cisco_pm_function(site)
     else: print("False")
